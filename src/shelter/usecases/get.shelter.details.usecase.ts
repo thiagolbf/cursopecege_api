2e463fd38d0d90a0d870cd9aa74d9a484164c217
Thiago Barcelos <thiagolbf@hotmail.com>
2024-04-03 20:19:00 -0300

    Criação de Projeto Base e Controllers

---
+++ src/shelter/usecases/get.shelter.details.usecase.ts
@@ -0,0 +1,19 @@
+import { IUseCase } from 'src/domain/iusecase.interface';
+import GetShelterDetailsUseCaseOutput from './dtos/get.shelter.details.usecase.output';
+
+export default class GetShelterDetailsUseCase
+  implements IUseCase<null, GetShelterDetailsUseCaseOutput>
+{
+  run(input: null): Promise<GetShelterDetailsUseCaseOutput> {
+    return Promise.resolve(
+      new GetShelterDetailsUseCaseOutput({
+        shelterName: 'Abrigo da luz',
+        shelterEmail: 'abrigodl@gmail.com',
+        shelterPhone: '19998745231',
+        shelterWhatsApp: '19998745231',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }),
+    );
+  }
+}
