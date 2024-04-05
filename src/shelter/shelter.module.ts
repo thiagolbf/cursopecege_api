304e3431819a7975133e7f992e99bfd8a6ce0938
Thiago Barcelos <thiagolbf@hotmail.com>
2024-04-04 22:33:22 -0300

    feat: criado conexão com banco dados mongo

--- src/shelter/shelter.module.ts
+++ src/shelter/shelter.module.ts
@@ -3,8 +3,16 @@ import { ShelterController } from './shelter.controller';
 import ShelterTokens from './shelter.tokens';
 import GetShelterDetailsUseCase from './usecases/get.shelter.details.usecase';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { Shelter, ShelterSchema } from './schemas/shelter.schema';
+
 @Module({
   controllers: [ShelterController],
+
+  imports: [
+    MongooseModule.forFeature([{ name: Shelter.name, schema: ShelterSchema }]),
+  ],
+
   providers: [
     {
       provide: ShelterTokens.getShelterDetailsUseCase,
