2673b9bf674b3d6a2b03cc735010083b8b5cbb58
Thiago Barcelos <thiagolbf@hotmail.com>
2024-04-08 22:38:12 -0300

    feat: criado controller de pet e o schema

---
+++ src/pet/pet.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import CreatePetControllerInput from './dtos/create.pet.controller.input';
+import CreatePetUseCaseOutput from './usecases/dtos/create.pet.usecase.output';
+import CreatePetUseCaseInput from './usecases/dtos/create.pet.usecase.input';
+import { IUseCase } from 'src/domain/iusecase.interface';
+import PetTokens from './pet.tokens';
+
+@Controller('pet')
+export class PetController {
+  @Inject(PetTokens.createPetUseCase)
+  private readonly createPetUseCase: IUseCase<
+    CreatePetUseCaseInput,
+    CreatePetUseCaseOutput
+  >;
+
+  @Post()
+  async createPet(
+    @Body() input: CreatePetControllerInput,
+  ): Promise<CreatePetUseCaseOutput> {
+    const useCaseInput = new CreatePetUseCaseInput({ ...input });
+
+    return await this.createPetUseCase.run(useCaseInput);
+  }
+}
